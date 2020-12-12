acecd8b8da36d79e641a19048c82c17e57667bfb
bnikhil011 <bnikhil011@gmail.com>
2020-12-12 14:51:40 +0530

    initial Setup for hibernate+ servlet project is done

---
+++ src/servlet_Services/AddServlet.java
@@ -0,0 +1,21 @@
+package servlet_Services;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+	@Override
+	public void service(HttpServletRequest req , HttpServletResponse res)
+	{
+		try {
+			res.getWriter().print("Will send the Reponse");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
